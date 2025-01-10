3af4f1aec217eff359c5a1f5fdf65f42863f383d
nictheboy <nictheboy@outlook.com>
2025-01-10 19:48:25 +0800

    Upload dependency: miniSTL

--- miniSTL/include/miniSTL/list.hpp
+++ miniSTL/include/miniSTL/list.hpp
@@ -1,29 +1,13 @@
 #pragma once
 
+#include <compare>
 #include <cstddef>
+#include <initializer_list>
 #include <iterator>
-#include <memory>
 #include <limits>
+#include <memory>
 #include <stdexcept>
 #include <utility>
-#include <compare>
-#include <initializer_list>
-
-template <class T>
-struct ListBaseNode {
-    ListBaseNode *m_next;
-    ListBaseNode *m_prev;
-
-    inline T &value();
-    inline T const &value() const;
-};
-
-template <class T>
-struct ListValueNode : ListBaseNode<T> {
-    union {
-        T m_value;
-    };
-};
 
 template <class T, class Alloc = std::allocator<T>>
 struct List {
@@ -31,183 +15,335 @@ struct List {
     using allocator_type = Alloc;
     using size_type = size_t;
     using difference_type = ptrdiff_t;
-    using pointer = T *;
-    using const_pointer = T const *;
-    using reference = T &;
-    using const_reference = T const &;
+    using pointer = T*;
+    using const_pointer = T const*;
+    using reference = T&;
+    using const_reference = T const&;
 
-private:
-    using ListNode = ListBaseNode<T>;
-    using AllocNode = std::allocator_traits<Alloc>::template rebind_alloc<ListValueNode<T>>;
+   private:
+    struct ListNode {
+        ListNode* m_next;
+        ListNode* m_prev;
+        union {
+            T m_value;
+        };
+
+        ListNode(ListNode* next, ListNode* prev)
+            : ListNode(next, prev, T()) {}
+        ListNode(ListNode* next, ListNode* prev, T const& value)
+            : m_next(next), m_prev(prev), m_value(value) {}
+        T& value() { return m_value; }
+        T const& value() const { return m_value; }
+    };
+    using AllocNode = std::allocator_traits<Alloc>::template rebind_alloc<ListNode>;
 
     ListNode m_dummy;
     size_t m_size;
-    [[no_unique_address]] Alloc m_alloc;
+    [[no_unique_address]] AllocNode m_alloc;
 
-public:
-    List();
+   public:
+    List()
+        : m_dummy(&m_dummy, &m_dummy), m_size(0), m_alloc() {}
 
-    List(List &&that);
+    List(List&& that) {
+        m_dummy = that.m_dummy;
+        m_size = that.m_size;
+        m_alloc = std::move(that.m_alloc);
+        that.m_dummy = ListValueNode(&that.m_dummy, &that.m_dummy);
+        that.m_size = 0;
+    }
 
-    List(List const &that);
+    List(List const& that)
+        : List() {
+        for (auto const& val : that)
+            push_back(val);
+    }
 
-    List &operator=(List const &that);
+    List& operator=(List const& that) {
+        clear();
+        for (auto const& val : that)
+            push_back(val);
+    }
 
-    bool empty();
+    bool empty() const { return size() == 0; }
 
-    T &front();
+    T& front() { return m_dummy.m_next->value(); }
 
-    T &back();
+    T& back() { return m_dummy.m_prev->value(); }
 
-    T const &front();
+    T const& front() const { return m_dummy.m_next->value(); }
 
-    T const &back();
+    T const& back() const { return m_dummy.m_prev->value(); }
 
-    explicit List(size_t n, Alloc const &alloc = Alloc());
+    explicit List(size_t n, Alloc const& alloc = Alloc())
+        : List(n, T(), alloc) {}
 
-    List(size_t n, T const &val, Alloc const &alloc = Alloc());
+    List(size_t n, T const& val, Alloc const& alloc = Alloc())
+        : m_dummy(&m_dummy, &m_dummy), m_size(0), m_alloc(alloc) {
+        assign(n, val);
+    }
 
     template <std::input_iterator InputIt>
-    List(InputIt first, InputIt last, Alloc const &alloc = Alloc());
+    List(InputIt first, InputIt last, Alloc const& alloc = Alloc())
+        : m_dummy(&m_dummy, &m_dummy), m_size(0), m_alloc(alloc) {
+        assign(first, last);
+    }
 
-    List(std::initializer_list<T> ilist, Alloc const &alloc = Alloc());
+    List(std::initializer_list<T> ilist, Alloc const& alloc = Alloc())
+        : List(ilist.begin(), ilist.end(), alloc) {}
 
-    List &operator=(std::initializer_list<T> ilist);
+    List& operator=(std::initializer_list<T> ilist) {
+        clear();
+        for (auto const& val : ilist)
+            push_back(val);
+    }
 
-    size_t size();
+    size_t size() const { return m_size; }
 
     template <std::input_iterator InputIt>
-    void assign(InputIt first, InputIt last);
+    void assign(InputIt first, InputIt last) {
+        clear();
+        for (auto it = first; it != last; ++it)
+            push_back(*it);
+    }
 
-    void assign(std::initializer_list<T> ilist);
+    void assign(std::initializer_list<T> ilist) {
+        assign(ilist.begin(), ilist.end());
+    }
 
-    void assign(size_t n, T const &val);
+    void assign(size_t n, T const& val) {
+        clear();
+        for (size_t i = 0; i < n; ++i)
+            push_back(val);
+    }
 
-    void push_back(T const &val);
+    void push_back(T const& val) {
+        iterator it = insert_node(cend());
+        *it = val;
+    }
 
-    void push_back(T &&val);
+    void push_back(T&& val) {
+        iterator it = insert_node(cend());
+        *it = std::move(val);
+    }
 
-    void push_front(T const &val);
+    void push_front(T const& val) {
+        iterator it = insert_node(cbegin());
+        *it = val;
+    }
 
-    void push_front(T &&val);
+    void push_front(T&& val) {
+        iterator it = insert_node(cbegin());
+        *it = std::move(val);
+    }
 
-    ~List();
+    ~List() {
+        clear();
+    }
 
-    void clear();
+    void clear() {
+        while (!empty())
+            pop_back();
+    }
 
     struct iterator {
         using iterator_category = std::bidirectional_iterator_tag;
         using value_type = T;
         using difference_type = ptrdiff_t;
-        using pointer = T *;
-        using reference = T &;
+        using pointer = T*;
+        using reference = T&;
 
-    private:
-        ListNode *m_curr;
+       private:
+        ListNode* m_curr;
 
         friend List;
 
-        explicit iterator(ListNode *curr);
+        explicit iterator(ListNode* curr)
+            : m_curr(curr) {}
 
-    public:
+       public:
         iterator() = default;
 
-        iterator &operator++();
+        iterator& operator++() {
+            m_curr = m_curr->m_next;
+            return *this;
+        }
 
-        iterator operator++(int);
+        iterator operator++(int) {
+            iterator copy = *this;
+            ++*this;
+            return copy;
+        }
 
-        iterator &operator--();
+        iterator& operator--() {
+            m_curr = m_curr->m_prev;
+            return *this;
+        }
 
-        iterator operator--(int);
+        iterator operator--(int) {
+            iterator copy = *this;
+            --*this;
+            return copy;
+        }
 
-        T &operator*();
+        T& operator*() { return m_curr->value(); }
 
-        bool operator!=(iterator const &that);
+        bool operator!=(iterator const& that) const { return m_curr != that.m_curr; }
 
-        bool operator==(iterator const &that);
+        bool operator==(iterator const& that) const { return m_curr == that.m_curr; }
     };
 
     struct const_iterator {
         using iterator_category = std::bidirectional_iterator_tag;
         using value_type = T;
         using difference_type = ptrdiff_t;
-        using pointer = T const *;
-        using reference = T const &;
+        using pointer = T const*;
+        using reference = T const&;
 
-    private:
-        ListNode const *m_curr;
+       private:
+        ListNode const* m_curr;
 
         friend List;
 
-        explicit const_iterator(ListNode const *curr);
+        explicit const_iterator(ListNode const* curr)
+            : m_curr(curr) {}
 
-    public:
-        const_iterator();
+       public:
+        const_iterator()
+            : const_iterator(nullptr) {}
 
-        const_iterator(iterator that);
+        const_iterator(iterator that)
+            : const_iterator(that.m_curr) {}
 
-        explicit operator iterator();
+        explicit operator iterator() { return iterator((ListNode*)m_curr); }
 
-        const_iterator &operator++();
+        const_iterator& operator++() {
+            m_curr = m_curr->m_next;
+            return *this;
+        }
 
-        const_iterator operator++(int);
+        const_iterator operator++(int) {
+            const_iterator copy = *this;
+            ++*this;
+            return copy;
+        }
 
-        const_iterator &operator--();
+        const_iterator& operator--() {
+            m_curr = m_curr->m_prev;
+            return *this;
+        }
 
-        const_iterator operator--(int);
+        const_iterator operator--(int) {
+            const_iterator copy = *this;
+            --*this;
+            return copy;
+        }
 
-        T const &operator*();
+        T const& operator*() { return m_curr->value(); }
 
-        bool operator!=(const_iterator const &that);
+        bool operator!=(const_iterator const& that) const { return m_curr != that.m_curr; }
 
-        bool operator==(const_iterator const &that);
+        bool operator==(const_iterator const& that) const { return m_curr == that.m_curr; }
     };
 
-    iterator begin();
+   private:
+    iterator insert_node(const_iterator pos) {
+        ListNode* node = m_alloc.allocate(1);
+        std::allocator_traits<AllocNode>::construct(m_alloc, node, iterator(pos).m_curr, iterator(pos).m_curr->m_prev);
+        iterator(pos).m_curr->m_prev->m_next = node;
+        iterator(pos).m_curr->m_prev = node;
+        ++m_size;
+        return iterator(node);
+    }
 
-    iterator end();
+   public:
+    iterator begin() { return iterator(m_dummy.m_next); }
 
-    const_iterator cbegin();
+    iterator end() { return iterator(&m_dummy); }
 
-    const_iterator cend();
+    const_iterator cbegin() const { return const_iterator(m_dummy.m_next); }
 
-    const_iterator begin();
+    const_iterator cend() const { return const_iterator(&m_dummy); }
 
-    const_iterator end();
+    const_iterator begin() const { return cbegin(); }
+
+    const_iterator end() const { return cend(); }
 
     using reverse_iterator = std::reverse_iterator<iterator>;
     using reverse_const_iterator = std::reverse_iterator<const_iterator>;
 
-    reverse_iterator rbegin();
+    reverse_iterator rbegin() { return reverse_iterator(end()); }
 
-    reverse_iterator rend();
+    reverse_iterator rend() { return reverse_iterator(begin()); }
 
-    reverse_const_iterator crbegin();
+    reverse_const_iterator crbegin() const { return reverse_const_iterator(cend()); }
 
-    reverse_const_iterator crend();
+    reverse_const_iterator crend() const { return reverse_const_iterator(cbegin()); }
 
-    reverse_const_iterator rbegin();
+    reverse_const_iterator rbegin() const { return crbegin(); }
 
-    reverse_const_iterator rend();
+    reverse_const_iterator rend() const { return crend(); }
 
-    iterator erase(const_iterator pos);
+    iterator erase(const_iterator pos) {
+        ListNode* next = pos.m_curr->m_next;
+        pos.m_curr->m_prev->m_next = next;
+        next->m_prev = pos.m_curr->m_prev;
 
-    iterator erase(const_iterator first, const_iterator last);
+        std::allocator_traits<AllocNode>::destroy(m_alloc, pos.m_curr);
+        m_alloc.deallocate((ListNode*)pos.m_curr, 1);
+        --m_size;
+        return iterator(next);
+    }
 
-    void pop_front();
+    iterator erase(const_iterator first, const_iterator last) {
+        while (first != last)
+            first = erase(first);
+        return iterator(first);
+    }
 
-    void pop_back();
+    void pop_front() {
+        erase(cbegin());
+    }
 
-    iterator insert(const_iterator pos, const T &val);
+    void pop_back() {
+        erase(--cend());
+    }
 
-    iterator insert(const_iterator pos, T &&val);
+    iterator insert(const_iterator pos, const T& val) {
+        iterator it = insert_node(pos);
+        *it = val;
+        return it;
+    }
 
-    iterator insert(const_iterator pos, size_t n, T const &val);
+    iterator insert(const_iterator pos, T&& val) {
+        iterator it = insert_node(pos);
+        *it = std::move(val);
+        return it;
+    }
+
+    iterator insert(const_iterator pos, size_t n, T const& val) {
+        for (size_t i = 0; i < n; ++i)
+            pos = insert(pos, val);
+        return pos;
+    }
 
     template <std::input_iterator InputIt>
-    iterator insert(const_iterator pos, InputIt first, InputIt last);
+    iterator insert(const_iterator pos, InputIt first, InputIt last) {
+        for (auto it = first; it != last; ++it)
+            pos = ++insert(pos, *it);
+        return --pos;
+    }
 
-    iterator insert(const_iterator pos, std::initializer_list<T> ilist);
+    iterator insert(const_iterator pos, std::initializer_list<T> ilist) {
+        return insert(pos, ilist.begin(), ilist.end());
+    }
 
-    bool operator==(List const &that);
+    bool operator==(List const& that) const {
+        if (size() != that.size())
+            return false;
+        for (auto it = begin(), that_it = that.begin(); it != end(); ++it, ++that_it)
+            if (*it != *that_it)
+                return false;
+        return true;
+    }
 };
--- miniSTL/include/miniSTL/vector.hpp
+++ miniSTL/include/miniSTL/vector.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
+#include <compare>
 #include <cstddef>
+#include <initializer_list>
 #include <iterator>
-#include <memory>
 #include <limits>
+#include <memory>
 #include <stdexcept>
 #include <utility>
-#include <compare>
-#include <initializer_list>
+#include <assert.h>
 
 template <class T, class Alloc = std::allocator<T>>
 struct Vector {
@@ -15,133 +16,286 @@ struct Vector {
     using allocator_type = Alloc;
     using size_type = size_t;
     using difference_type = ptrdiff_t;
-    using pointer = T *;
-    using const_pointer = T const *;
-    using reference = T &;
-    using const_reference = T const &;
-    using iterator = T *;
-    using const_iterator = T const *;
-    using reverse_iterator = std::reverse_iterator<T *>;
-    using const_reverse_iterator = std::reverse_iterator<T const *>;
+    using pointer = T*;
+    using const_pointer = T const*;
+    using reference = T&;
+    using const_reference = T const&;
+    using iterator = T*;
+    using const_iterator = T const*;
+    using reverse_iterator = std::reverse_iterator<T*>;
+    using const_reverse_iterator = std::reverse_iterator<T const*>;
 
-    T *m_data;
+    T* m_data;
     size_t m_size;
     size_t m_cap;
     [[no_unique_address]] Alloc m_alloc;
 
-    Vector();
+   private:
+    void initialize(size_t n, Alloc const& alloc) {
+        m_data = ((Alloc&)alloc).allocate(n);
+        m_size = 0;
+        m_cap = n;
+        m_alloc = alloc;
+    }
 
-    Vector(std::initializer_list<T> ilist, Alloc const &alloc = Alloc());
+    void reallocate(int n) {
+        assert(n >= m_size);
+        T* old = m_data;
+        m_data = m_alloc.allocate(n);
+        for (int i = 0; i < m_size; i++)
+            m_data[i] = std::move(old[i]);
+        m_alloc.deallocate(old, m_cap);
+        m_cap = n;
+    }
 
-    explicit Vector(size_t n, Alloc const &alloc = Alloc());
+    void ensure_space() {
+        if (m_size == m_cap)
+            reallocate(m_cap == 0 ? 1 : m_cap * 2);
+    }
 
-    Vector(size_t n, T const &val, Alloc const &alloc = Alloc());
+    T* insert_space(T const* it, size_t n) {
+        size_t index = it - m_data;
+        reallocate(m_size + n);
+        T* it2 = m_data + index;
+        for (T* i = end() - 1; i != it2; i--)
+            *i = std::move(*(i - n));
+        m_size += n;
+        return it2;
+    }
 
-    template <std::random_access_iterator InputIt>
-    Vector(InputIt first, InputIt last, Alloc const &alloc = Alloc());
+   public:
+    Vector()
+        : Vector(0, T()) {}
 
-    void clear();
+    Vector(std::initializer_list<T> ilist, Alloc const& alloc = Alloc()) {
+        initialize(ilist.size(), alloc);
+        assign(ilist);
+    }
 
-    void resize(size_t n);
+    explicit Vector(size_t n, Alloc const& alloc = Alloc())
+        : Vector(n, T(), alloc) {}
 
-    void resize(size_t n, T const &val);
-
-    void shrink_to_fit();
-
-    void reserve(size_t n);
-
-    size_t capacity();
-
-    size_t size();
-
-    bool empty();
-
-    T const &operator[](size_t i);
-
-    T &operator[](size_t i);
-
-    T const &at(size_t i);
-
-    T &at(size_t i);
-
-    Vector(Vector &&that);
-
-    Vector(Vector &&that, Alloc const &alloc);
-    Vector &operator=(Vector &&that);
-
-    void swap(Vector &that);
-
-    Vector(Vector const &that);
-
-    Vector(Vector const &that, Alloc const &alloc);
-
-    Vector &operator=(Vector const &that);
-
-    T const &front();
-
-    T &front();
-
-    T const &back();
-
-    T &back();
-
-    void push_back(T const &val);
-
-    void push_back(T &&val);
-
-    T *data();
-
-    T const *data();
-
-    T const *cdata();
-
-    T *begin();
-
-    T *end();
-
-    T const *begin();
-
-    T const *end();
-
-    T const *cbegin();
-
-    T const *cend();
-
-    std::reverse_iterator<T *> rbegin();
-
-    std::reverse_iterator<T *> rend();
-
-    std::reverse_iterator<T const *> crbegin();
-
-    std::reverse_iterator<T const *> crend();
-
-    void pop_back();
-
-    T *erase(T const *it) noexcept(std::is_nothrow_move_assignable_v<T>);
-
-    T *erase(T const *first, T const *last) noexcept(std::is_nothrow_move_assignable_v<T>);
-
-    void assign(size_t n, T const &val);
+    Vector(size_t n, T const& val, Alloc const& alloc = Alloc()) {
+        initialize(n, alloc);
+        assign(n, val);
+    }
 
     template <std::random_access_iterator InputIt>
-    void assign(InputIt first, InputIt last);
+    Vector(InputIt first, InputIt last, Alloc const& alloc = Alloc()) {
+        initialize(last - first, alloc);
+        assign(first, last);
+    }
 
-    void assign(std::initializer_list<T> ilist);
+    void clear() {
+        while (!empty())
+            pop_back();
+        reallocate(0);
+    }
 
-    Vector &operator=(std::initializer_list<T> ilist);
+    void resize(size_t n) {
+        resize(n, T());
+    }
 
-    T *insert(T const *it, T &&val);
+    void resize(size_t n, T const& val) {
+        while (size() > n)
+            pop_back();
+        reallocate(n);
+        while (size() < n)
+            push_back(val);
+    }
 
-    T *insert(T const *it, T const &val);
+    void shrink_to_fit() { reallocate(m_size); }
 
-    T *insert(T const *it, size_t n, T const &val);
+    void reserve(size_t n) {
+        if (n <= m_cap)
+            return;
+        reallocate(n);
+    }
+
+    size_t capacity() const { return m_cap; }
+
+    size_t size() const { return m_size; }
+
+    bool empty() const { return size() == 0; }
+
+    T const& operator[](size_t i) const { return m_data[i]; }
+
+    T& operator[](size_t i) { return m_data[i]; }
+
+    T const& at(size_t i) const { return m_data[i]; }
+
+    T& at(size_t i) { return m_data[i]; }
+
+    Vector(Vector&& that)
+        : Vector(that, that.m_alloc) {}
+
+    Vector(Vector&& that, Alloc const& alloc) {
+        initialize(that.m_size, alloc);
+        for (auto iterator = that.begin(); iterator != that.end(); iterator++)
+            push_back(*iterator);
+    }
+
+    Vector& operator=(Vector&& that) {
+        ~Vector();
+        assign(that.begin(), that.end());
+    }
+
+    void swap(Vector& that) {
+        T* data = m_data;
+        size_t size = m_size;
+        size_t cap = m_cap;
+        Alloc alloc = m_alloc;
+        m_data = that.m_data;
+        m_size = that.m_size;
+        m_cap = that.m_cap;
+        m_alloc = that.m_alloc;
+        that.m_data = data;
+        that.m_size = size;
+        that.m_cap = cap;
+        that.m_alloc = alloc;
+    }
+
+    Vector(Vector const& that)
+        : Vector(that) {}
+
+    Vector(Vector const& that, Alloc const& alloc)
+        : Vector(that, alloc) {}
+
+    Vector& operator=(Vector const& that) {
+        ~Vector();
+        assign(that.begin(), that.end());
+    }
+
+    T const& front() const { return m_data[0]; }
+
+    T& front() { return m_data[0]; }
+
+    T const& back() const { return m_data[m_size - 1]; }
+
+    T& back() { return m_data[m_size - 1]; }
+
+    void push_back(T const& val) {
+        ensure_space();
+        m_data[m_size++] = val;
+    }
+
+    void push_back(T&& val) {
+        ensure_space();
+        m_data[m_size++] = std::move(val);
+    }
+
+    T* data() { return m_data; }
+
+    T const* data() const { return m_data; }
+
+    T const* cdata() const { return m_data; }
+
+    T* begin() { return m_data; }
+
+    T* end() { return m_data + m_size; }
+
+    T const* begin() const { return m_data; }
+
+    T const* end() const { return m_data + m_size; }
+
+    T const* cbegin() const { return m_data; }
+
+    T const* cend() const { return m_data + m_size; }
+
+    std::reverse_iterator<T*> rbegin() { return std::reverse_iterator<T*>(end()); }
+
+    std::reverse_iterator<T*> rend() { return std::reverse_iterator<T*>(begin()); }
+
+    std::reverse_iterator<T const*> crbegin() { return std::reverse_iterator<T const*>(cend()); }
+
+    std::reverse_iterator<T const*> crend() { return std::reverse_iterator<T const*>(cbegin()); }
+
+    void pop_back() {
+        assert(!empty());
+        m_size--;
+    }
+
+    T* erase(T const* it) noexcept(std::is_nothrow_move_assignable_v<T>) {
+        return erase(it, it + 1);
+    }
+
+    T* erase(T const* first, T const* last) noexcept(std::is_nothrow_move_assignable_v<T>) {
+        assert(first <= last);
+        assert(last <= end());
+        size_t n = last - first;
+        for (T* it = first - m_data + m_data; it != last; it++)
+            it->~T();
+        for (T* it = first - m_data + m_data; it != end() - n; it++)
+            *it = std::move(*(it + n));
+        m_size -= n;
+        return first - m_data + m_data;
+    }
+
+    void assign(size_t n, T const& val) {
+        clear();
+        resize(n, val);
+    }
 
     template <std::random_access_iterator InputIt>
-    T *insert(T const *it, InputIt first, InputIt last);
+    void assign(InputIt first, InputIt last) {
+        clear();
+        for (InputIt it = first; it != last; it++)
+            push_back(*it);
+    }
 
-    T *insert(T const *it, std::initializer_list<T> ilist);
+    void assign(std::initializer_list<T> ilist) {
+        clear();
+        for (T const& val : ilist)
+            push_back(val);
+    }
 
-    ~Vector();
+    Vector& operator=(std::initializer_list<T> ilist) {
+        assign(ilist);
+    }
 
-    bool operator==(Vector const &that);
+    T* insert(T const* it, T&& val) {
+        T* it2 = insert_space(it, 1);
+        *it2 = std::move(val);
+        return it2;
+    }
+
+    T* insert(T const* it, T const& val) {
+        T* it2 = insert_space(it, 1);
+        *it2 = val;
+        return it2;
+    }
+
+    T* insert(T const* it, size_t n, T const& val) {
+        T* it2 = insert_space(it, n);
+        for (size_t i = 0; i < n; i++)
+            it2[i] = val;
+        return it2;
+    }
+
+    template <std::random_access_iterator InputIt>
+    T* insert(T const* it, InputIt first, InputIt last) {
+        size_t n = last - first;
+        T* it2 = insert_space(it, n);
+        for (size_t i = 0; i < n; i++)
+            it2[i] = *first++;
+        return it2;
+    }
+
+    T* insert(T const* it, std::initializer_list<T> ilist) {
+        return insert(it, ilist.begin(), ilist.end());
+    }
+
+    ~Vector() {
+        clear();
+        m_alloc.deallocate(m_data, m_cap);
+    }
+
+    bool operator==(Vector const& that) const {
+        if (size() != that.size())
+            return false;
+        for (size_t i = 0; i < size(); i++)
+            if (!(m_data[i] == that.m_data[i]))
+                return false;
+        return true;
+    }
 };
