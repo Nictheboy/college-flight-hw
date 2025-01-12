f87608ea1b0303d2fa9d5a891557481fb9bbd5bb
nictheboy <nictheboy@outlook.com>
2025-01-12 23:06:13 +0800

    refactor: remove redundant files

--- project/include/flight_graph_airport_only.hpp
+++ project/include/flight_graph_airport_only.hpp
@@ -7,9 +7,26 @@ class FlightGraphAirportOnly : public AbstractFlightGraph {
     std::shared_ptr<FlightDatabase> flight_database;
 
    public:
-    FlightGraphAirportOnly(std::shared_ptr<FlightDatabase> flight_database);
+    FlightGraphAirportOnly(std::shared_ptr<FlightDatabase> flight_database)
+        : AbstractFlightGraph(flight_database->AirportRange()), flight_database(flight_database) {}
 
    protected:
-    virtual std::shared_ptr<Vector<EdgeKey>> DiscreteChildrenOf(FlightNodeKey node) const override;
-    virtual bool IsContinuousChild(FlightNodeKey parent, FlightNodeKey child) const override;
+    virtual std::shared_ptr<Vector<EdgeKey>> DiscreteChildrenOf(FlightNodeKey node) const override {
+        auto airport = node.airport;
+        auto no_sooner_than = node.no_sooner_than;
+        auto record_ids = flight_database->QueryRecordIdsByAirportFrom(airport);
+        auto edge_keys = std::make_shared<Vector<EdgeKey>>();
+        edge_keys->reserve(record_ids->size());
+        for (auto record_id : *record_ids) {
+            auto record = flight_database->QueryRecordById(record_id);
+            edge_keys->push_back({{record.airport_to, 0}, 0});
+        }
+        auto last = std::unique(edge_keys->begin(), edge_keys->end());
+        edge_keys->erase(last, edge_keys->end());
+        return edge_keys;
+    }
+
+    virtual bool IsContinuousChild(FlightNodeKey parent, FlightNodeKey child) const override {
+        return false;
+    }
 };
--- project/include/flight_graph_complete.hpp
+++ project/include/flight_graph_complete.hpp
@@ -7,10 +7,27 @@ class FlightGraphComplete : public AbstractFlightGraph {
     std::shared_ptr<FlightDatabase> flight_database;
 
    public:
-    FlightGraphComplete(std::shared_ptr<FlightDatabase> flight_database);
+    FlightGraphComplete(std::shared_ptr<FlightDatabase> flight_database)
+        : AbstractFlightGraph(flight_database->AirportRange()), flight_database(flight_database) {}
 
    protected:
     virtual int Weight(FlightNodeKey from, FlightDatabase::Record& record) const { return 0; }
-    virtual std::shared_ptr<Vector<EdgeKey>> DiscreteChildrenOf(FlightNodeKey node) const override;
-    virtual bool IsContinuousChild(FlightNodeKey parent, FlightNodeKey child) const override;
+    virtual std::shared_ptr<Vector<EdgeKey>> DiscreteChildrenOf(FlightNodeKey node) const override {
+        auto airport = node.airport;
+        auto no_sooner_than = node.no_sooner_than;
+        auto record_ids = flight_database->QueryRecordIdsByAirportFrom(airport);
+        auto edge_keys = std::make_shared<Vector<EdgeKey>>();
+        edge_keys->reserve(record_ids->size());
+        for (auto record_id : *record_ids) {
+            auto record = flight_database->QueryRecordById(record_id);
+            if (record.datetime_from < no_sooner_than)
+                continue;
+            edge_keys->push_back({{record.airport_to, record.datetime_to}, Weight(node, record)});
+        }
+        return edge_keys;
+    }
+
+    virtual bool IsContinuousChild(FlightNodeKey parent, FlightNodeKey child) const override {
+        return parent.airport == child.airport && parent.no_sooner_than <= child.no_sooner_than;
+    }
 };
